a37a70bef63d2f00b7a252c83b60911d427bcb3b
varun-Tad <varuntad141@gmail.com>
2022-05-07 01:28:41 +0530

    added search feature

---
+++ monster-rolo/src/App.js
@@ -0,0 +1,59 @@
+import { Component } from "react";
+
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    return (
+      <div className="App">
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="search Monsters"
+          className="search-box"
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
