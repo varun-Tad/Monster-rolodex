f600c8f472a82cf8c5ed009e4f8c07c7652125ba
varun-Tad <varuntad141@gmail.com>
2022-05-07 18:16:14 +0530

    updated App.js

--- monster-rolo/src/App.js
+++ monster-rolo/src/App.js
@@ -39,57 +39,6 @@ const App = () => {
       <CardList monsters={filteredMonsters} />
     </div>
   );
-  // };
-  // class App extends Component {
-  //   constructor() {
-  //     super();
-  //     this.state = {
-  //       monsters: [],
-  //       searchField: "",
-  //     };
-  //   }
-
-  // componentDidMount() {
-  //   fetch("https://jsonplaceholder.typicode.com/users")
-  //     .then((response) => response.json())
-  //     .then((users) =>
-  //       this.setState(
-  //         () => {
-  //           return { monsters: users };
-  //         },
-  //         () => {
-  //           console.log(this.state);
-  //         }
-  //       )
-  //     );
-  // }
-
-  // onSearchChange = (event) => {
-  //   const searchField = event.target.value.toLocaleLowerCase();
-
-  //   this.setState(() => {
-  //     return { searchField };
-  //   });
-  // };
-
-  // render() {
-  //   const { monsters, searchField } = this.state;
-  //   const { onSearchChange } = this;
-  //   const filteredMonsters = monsters.filter((monster) => {
-  //     return monster.name.toLocaleLowerCase().includes(searchField);
-  //   });
-  //   return (
-  //     <div className="App">
-  //       <h1 className="app-title">Monsters Rolodex</h1>
-  //       <SearchBox
-  //         onChangeHandler={onSearchChange}
-  //         placeholder="search Monsters"
-  //         className="search-box"
-  //       />
-  //       <CardList monsters={filteredMonsters} />
-  //     </div>
-  //   );
-  // }
 };
 
 export default App;
